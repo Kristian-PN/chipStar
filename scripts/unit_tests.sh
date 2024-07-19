af0837b5d53f4750ae0477179ebb508e8d655607
Kristian Kadlubiak <kkristian@pglc.io>
2024-07-19 19:03:29 +0000

    Modified unit_tests to work with Salami

--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -2,12 +2,18 @@
 
 set -e
 
-# read the file /opt/actions-runner/num-threads.txt and set the number of threads to the value in the file
+host=`hostname`
+echo "Running on ${hostname}"
+# If not on Salami read the file /opt/actions-runner/num-threads.txt and set the number of threads to the value in the file
 # if the file does not exist, set the number of threads to 24
-if [ -f "/opt/actions-runner/num-threads.txt" ]; then
-  num_threads=$(cat /opt/actions-runner/num-threads.txt)
+if [ "$host" = "salami" ]; then
+  num_threads=1
 else
-  num_threads=$(nproc)
+  if [ -f "/opt/actions-runner/num-threads.txt" ]; then
+    num_threads=$(cat /opt/actions-runner/num-threads.txt)
+  else
+    num_threads=$(nproc)
+  fi
 fi
 
 num_tries=1
@@ -29,22 +35,33 @@ fi
 # Set the build type based on the argument
 build_type=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 
-if [ "$2" == "llvm-15" ]; then
-  LLVM=llvm-15
-  CLANG=llvm/15.0-unpatched-spirv
-elif [ "$2" == "llvm-16" ]; then
-  LLVM=llvm-16
-  CLANG=llvm/16.0-unpatched-spirv
-elif [ "$2" == "llvm-17" ]; then
-  LLVM=llvm-17
-  CLANG=llvm/17.0-unpatched-spirv
-elif [ "$2" == "llvm-18" ]; then
-  LLVM=llvm-18
-  CLANG=llvm/18.0-unpatched-spirv
+if [ "$host" = "salami" ]; then
+  if [ "$2" == "llvm-15" ]; then
+    LLVM=llvm-15
+    CLANG=llvm/15.0-exts-only
+  else
+    echo "$2"
+    echo "Invalid 2nd argument. Use  'llvm-15'."
+    exit 1
+  fi
 else
-  echo "$2"
-  echo "Invalid 2nd argument. Use either 'llvm-15', 'llvm-16', 'llvm-17' or 'llvm-18'."
-  exit 1
+  if [ "$2" == "llvm-15" ]; then
+    LLVM=llvm-15
+    CLANG=llvm/15.0-unpatched-spirv
+  elif [ "$2" == "llvm-16" ]; then
+    LLVM=llvm-16
+    CLANG=llvm/16.0-unpatched-spirv
+  elif [ "$2" == "llvm-17" ]; then
+    LLVM=llvm-17
+    CLANG=llvm/17.0-unpatched-spirv
+  elif [ "$2" == "llvm-18" ]; then
+    LLVM=llvm-18
+    CLANG=llvm/18.0-unpatched-spirv
+  else
+    echo "$2"
+    echo "Invalid 2nd argument. Use either 'llvm-15', 'llvm-16', 'llvm-17' or 'llvm-18'."
+    exit 1
+  fi
 fi
 
 shift
@@ -100,7 +117,9 @@ echo "skip_build  = ${skip_build}"
 echo "timeout     = ${timeout}"
 
 # source /opt/intel/oneapi/setvars.sh intel64 &> /dev/null
-source /etc/profile.d/modules.sh &> /dev/null
+if [ "$host" != "salami" ]; then
+  source /etc/profile.d/modules.sh &> /dev/null
+fi
 export IGC_EnableDPEmulation=1
 export OverrideDefaultFP64Settings=1
 export CHIP_LOGLEVEL=err
@@ -110,8 +129,13 @@ export POCL_KERNEL_CACHE=0
 export CHIP_L0_EVENT_TIMEOUT=$(($timeout - 10))
 
 # Use OpenCL for building/test discovery to prevent Level Zero from being used in multi-thread/multi-process environment
-module use ~/modulefiles
-module load oneapi/2024.1.0 $CLANG opencl/dgpu 
+if [ "$host" = "salami" ]; then
+  module use  /home/kristian/apps/modulefiles
+  module load $CLANG
+else
+  module use ~/modulefiles
+  module load oneapi/2024.1.0 $CLANG opencl/dgpu 
+fi
 
 output=$(clinfo -l 2>&1 | grep "Platform #0")
 echo $output
@@ -130,6 +154,13 @@ if [ $skip_build ]; then
   echo "Skipping build step"
   cd build
 else
+  if [ "$host" = "salami" ]; then
+    CHIP_OPTIONS="-DCHIP_MALI_GPU_WORKAROUNDS=ON -DCHIP_SKIP_TESTS_WITH_DOUBLES=ON"
+    build_threads=1
+  else
+    CHIP_OPTIONS="-DCHIP_BUILD_HIPBLAS=ON"
+    build_threads=`nproc`
+  fi
   # Build the project
   echo "Building project..."
   rm -rf HIPCC
@@ -145,47 +176,62 @@ else
   cd build
 
   echo "building with $CLANG"
-  cmake ../ -DCMAKE_BUILD_TYPE="$build_type" -DCHIP_BUILD_HIPBLAS=ON
-  make all build_tests install -j $(nproc) #&> /dev/null
+  cmake ../ -DCMAKE_BUILD_TYPE="$build_type"  ${CHIP_OPTIONS}
+  make all build_tests install -j ${build_threads} #&> /dev/null
   echo "chipStar build complete." 
 fi
 
-module unload opencl/dgpu
+if [ "$host" != "salami" ]; then
+  module unload opencl/dgpu
+else
+
 
 # module load HIP/hipBLAS/main/release # for libCEED NOTE: Must be after build step otherwise it will cause link issues.
 
-# Test Level Zero iGPU
-echo "begin igpu_level0_failed_tests"
-# module load level-zero/igpu
-# module list
-../scripts/check.py ./ igpu level0 --num-threads=${num_threads} --timeout=$timeout --num-tries=$num_tries --modules=on | tee igpu_level0_make_check_result.txt
-# module unload level-zero/igpu
-echo "end igpu_level0_failed_tests"
 
-# Test Level Zero dGPU
-echo "begin dgpu_level0_failed_tests"
-# module load level-zero/dgpu
-# module list
-../scripts/check.py ./ dgpu level0 --num-threads=${num_threads} --timeout=$timeout --num-tries=$num_tries --modules=on | tee dgpu_level0_make_check_result.txt
-# module unload level-zero/dgpu
-echo "end dgpu_level0_failed_tests"
+if [ "$host" = "salami" ]; then
+  # Test OpenCL iGPU
+  echo "begin igpu_opencl_failed_tests"
+  # module load opencl/igpu
+  # module list
+  ../scripts/check.py ./ igpu opencl --num-threads=${num_threads} --timeout=$timeout --num-tries=$num_tries --modules=off | tee igpu_opencl_make_check_result.txt
+  # module unload opencl/igpu
+  echo "end igpu_opencl_failed_tests"
+else
+  # Test Level Zero iGPU
+  echo "begin igpu_level0_failed_tests"
+  # module load level-zero/igpu
+  # module list
+  ../scripts/check.py ./ igpu level0 --num-threads=${num_threads} --timeout=$timeout --num-tries=$num_tries --modules=on | tee igpu_level0_make_check_result.txt
+  # module unload level-zero/igpu
+  echo "end igpu_level0_failed_tests"
 
-# Test OpenCL iGPU
-echo "begin igpu_opencl_failed_tests"
-# module load opencl/igpu
-# module list
-../scripts/check.py ./ igpu opencl --num-threads=${num_threads} --timeout=$timeout --num-tries=$num_tries --modules=on | tee igpu_opencl_make_check_result.txt
-# module unload opencl/igpu
-echo "end igpu_opencl_failed_tests"
+  # Test Level Zero dGPU
+  echo "begin dgpu_level0_failed_tests"
+  # module load level-zero/dgpu
+  # module list
+  ../scripts/check.py ./ dgpu level0 --num-threads=${num_threads} --timeout=$timeout --num-tries=$num_tries --modules=on | tee dgpu_level0_make_check_result.txt
+  # module unload level-zero/dgpu
+  echo "end dgpu_level0_failed_tests"
 
-# Test OpenCL dGPU
-echo "begin dgpu_opencl_failed_tests"
-# module load intel/opencl # sets ICD
-# module load opencl/dgpu # sets CHIP_BE
-# module list
-../scripts/check.py ./ dgpu opencl --num-threads=${num_threads} --timeout=$timeout --num-tries=$num_tries --modules=on | tee dgpu_opencl_make_check_result.txt
-# module unload opencl/dgpu intel/opencl
-echo "end dgpu_opencl_failed_tests"
+  # Test OpenCL iGPU
+  echo "begin igpu_opencl_failed_tests"
+  # module load opencl/igpu
+  # module list
+  ../scripts/check.py ./ igpu opencl --num-threads=${num_threads} --timeout=$timeout --num-tries=$num_tries --modules=on | tee igpu_opencl_make_check_result.txt
+  # module unload opencl/igpu
+  echo "end igpu_opencl_failed_tests"
+
+  # Test OpenCL dGPU
+  echo "begin dgpu_opencl_failed_tests"
+  # module load intel/opencl # sets ICD
+  # module load opencl/dgpu # sets CHIP_BE
+  # module list
+  ../scripts/check.py ./ dgpu opencl --num-threads=${num_threads} --timeout=$timeout --num-tries=$num_tries --modules=on | tee dgpu_opencl_make_check_result.txt
+  # module unload opencl/dgpu intel/opencl
+  echo "end dgpu_opencl_failed_tests"
+fi
+
 
 function check_tests {
   file="$1"
