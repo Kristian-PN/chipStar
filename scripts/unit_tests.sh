ed58a3518d9bc3922346eec5ac13396c0c3f7198
Kristian Kadlubiak <kkristian@pglc.io>
2024-07-26 11:43:48 +0000

    Testing with custom LLVM module

--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -36,9 +36,9 @@ fi
 build_type=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 
 if [ "$host" = "salami" ]; then
-  if [ "$2" == "llvm-17" ]; then
-    LLVM=llvm-17
-    CLANG=llvm/17.0
+  if [ "$2" == "llvm-15" ]; then
+    LLVM=llvm-15
+    CLANG=llvm/15.0-exts-only
   else
     echo "$2"
     echo "Invalid 2nd argument. Use  'llvm-15'."
@@ -130,9 +130,8 @@ export CHIP_L0_EVENT_TIMEOUT=$(($timeout - 10))
 
 # Use OpenCL for building/test discovery to prevent Level Zero from being used in multi-thread/multi-process environment
 if [ "$host" = "salami" ]; then
-  pwd
-  # module use  /home/kristian/apps/modulefiles
-  # module load $CLANG
+  module use  /home/kristian/apps/modulefiles
+  module load $CLANG
 else
   module use ~/modulefiles
   module load oneapi/2024.1.0 $CLANG opencl/dgpu 
